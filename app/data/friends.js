a75810b2eb9176a5e5fdc56ca87dceb89c31841a
JMDahle <47642064+jmdahle@users.noreply.github.com>
2019-07-22 16:44:16 -0500

    full working version

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,35 +2,35 @@ var friends = [
     {
         "name": "Alice",
         "photo": "https://randomuser.me/api/portraits/women/71.jpg",
-        "surveryScores" :[
+        "surveyScores" :[
             1,3,5,4,2,5,4,3,2,1
         ]
     },
     {
         "name": "Ben",
         "photo": "https://randomuser.me/api/portraits/men/91.jpg",
-        "surveryScores" :[
-            1,2,3,4,5,5,4,3,2,1
+        "surveyScores" :[
+            5,2,3,4,5,5,4,3,2,1
         ]
     },
     {
         "name": "Callie",
         "photo": "https://randomuser.me/api/portraits/women/81.jpg",
-        "surveryScores" :[
+        "surveyScores" :[
             5,4,3,2,1,5,4,3,2,1
         ]
     },
     {
         "name": "Doug",
         "photo": "https://randomuser.me/api/portraits/men/51.jpg",
-        "surveryScores" :[
-            4,2,2,4,1,1,3,3,3,5
+        "surveyScores" :[
+            4,2,2,3,1,1,3,3,3,5
         ]
     },
     {
         "name": "Evelyn",
         "photo": "https://randomuser.me/api/portraits/women/31.jpg",
-        "surveryScores" :[
+        "surveyScores" :[
             3,3,3,4,4,1,1,5,5,4
         ]
     }
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,7 +8,39 @@ module.exports = function (app) {
         res.json(friends);
     });
     // 'post' path to 'api/friends' runs compatability check
-    app.post('./api/friends', (req, res) => {
-        res.send('Running compatability check');
+    app.post('/api/friends', (req, res) => {
+        let newPerson = req.body;
+        let isDuplicate = false;  // for excluding duplicate person
+        let duplicateIndex = -1; // for excluding duplicate person
+        // compare survey to friends        
+        let minDiff = 41;  // highest possible value is 4 * 10; since matches are tested based on the LOWEST difference, we can start with 1 greater than the maximum and always substitute with any lower one
+        let bestMatches = []; // an array for matches in case there is a tie/more than one
+        for (let i = 0; i < friends.length; i++) {
+            let diff = 0;
+            // skip if duplicate
+            if (friends[i].name === newPerson.name) {
+                isDuplicate = true;  // duplicates are skipped
+                duplicateIndex = i; // retain location of duplicate for excluding from being re-added
+            } else {
+                isDuplicate = false;  // no duplicate, evaluate compatability
+                for (p = 0; p < friends[i].surveyScores.length; p++) {
+                    diff += Math.abs(parseInt(friends[i].surveyScores[p]) - parseInt(newPerson.surveyScores[p]));
+                }
+                if (diff === minDiff) {
+                    // duplicate of lowest difference; add to the match array
+                    bestMatches.push(friends[i]);
+                } else if (diff < minDiff) {
+                    // new lowest difference; set array equal to this match and set new minimum difference
+                    bestMatches = [friends[i]];
+                    minDiff = diff;
+                }
+            }
+        }
+        // add newPerson (if not a duplicate)
+        if (duplicateIndex < 0) {
+            // add newPerson... not a duplicate
+            friends.push(newPerson);
+        }
+        res.send(bestMatches);
     });
 }
