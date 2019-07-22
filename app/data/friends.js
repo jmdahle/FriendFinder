70ff923b1c6f5ad84da99725bf259aa4422f4721
JMDahle <47642064+jmdahle@users.noreply.github.com>
2019-07-22 06:48:41 -0500

    survey structure working

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,35 +1,35 @@
 var friends = [
     {
         "name": "Alice",
-        "photo": "",
+        "photo": "https://randomuser.me/api/portraits/women/71.jpg",
         "surveryScores" :[
             1,3,5,4,2,5,4,3,2,1
         ]
     },
     {
         "name": "Ben",
-        "photo": "",
+        "photo": "https://randomuser.me/api/portraits/men/91.jpg",
         "surveryScores" :[
             1,2,3,4,5,5,4,3,2,1
         ]
     },
     {
         "name": "Callie",
-        "photo": "",
+        "photo": "https://randomuser.me/api/portraits/women/81.jpg",
         "surveryScores" :[
             5,4,3,2,1,5,4,3,2,1
         ]
     },
     {
         "name": "Doug",
-        "photo": "",
+        "photo": "https://randomuser.me/api/portraits/men/51.jpg",
         "surveryScores" :[
             4,2,2,4,1,1,3,3,3,5
         ]
     },
     {
         "name": "Evelyn",
-        "photo": "",
+        "photo": "https://randomuser.me/api/portraits/women/31.jpg",
         "surveryScores" :[
             3,3,3,4,4,1,1,5,5,4
         ]
